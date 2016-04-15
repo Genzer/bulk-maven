c6e0642c8812bb81a0f2c2c896ee9741d39122c9
genzers@gmail.com <genzers@gmail.com>
2016-04-15 17:05:08 +0700

    Added bulk-maven.bat version 1.0 and its documentation for usage

---
+++ bulk-maven.bat
@@ -0,0 +1,22 @@
+:: This is a small BATCH file which ease the use of mvn commands.
+:: version 1.0
+@echo OFF
+
+:: Note: the ~ character is for removing the quotes in arguments
+set maven_commands=%~1
+set specified_folders=%~2
+
+if "%maven_commands%"=="" if "%specified_folders%"=="" (
+	echo Usage: bulk-maven.bat ["maven-life-cycle-commands"] ["directory-patterns-separated-by-spaces"]
+	goto:eof
+)
+
+
+if "%specified_folders%"=="" (
+	:: If no projects specified, apply command to all 1st level sub-directory
+	set projects=(*)
+) else (
+	set projects=(%specified_folders%)
+)
+
+for /D %%G in %projects% do mvn -f %%G %maven_commands%
